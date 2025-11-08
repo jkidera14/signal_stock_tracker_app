54414ff7bbc17805e7a276bac67ad4a409221376
Jon Kid <35459963+jkidera14@users.noreply.github.com>
2025-11-08 18:24:52 +0300

    Implement home page with Trading View widgets

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -58,7 +58,8 @@ const user ={name: 'John', email: 'contact@jontemasta.com'};
                 <DropdownMenuSeparator className="bg-gray-600"/>
                 <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                     <LogOut className="h-4 w-4 mr-2 hidden sm:block"/>
-Logout        </DropdownMenuItem>
+                    Logout
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
