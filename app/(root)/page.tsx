9bd1111713f36e1ff792e23387e3728529cc01a3
Jon Kid <35459963+jkidera14@users.noreply.github.com>
2025-11-10 22:51:53 +0300

    Implement Auth UI & Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,14 +8,14 @@ import {
 
 const Page = () => {
 
-    const scritUrl=`https://s3.tradingview.com/external-embedding/embed-widget-`
+    const scriptUrl=`https://s3.tradingview.com/external-embedding/embed-widget-`
     return (
         <div className="flex min-h-screen home-wrapper">
             <section className= "grid w-full gap-8 home-section">
                 <div className="md:col-span-1 xl:col-span-1">
                     <TradingViewWidget
                     title="Market Overview"
-                    scriptUrl={`${scritUrl}market-overview.js`}
+                    scriptUrl={`${scriptUrl}market-overview.js`}
                     config={MARKET_OVERVIEW_WIDGET_CONFIG}
                     className="custom-chart"
                     height={600}
@@ -24,7 +24,7 @@ const Page = () => {
                 <div className="md-col-span xl:col-span-2">
                     <TradingViewWidget
                         title="Stock Heatmap"
-                        scriptUrl={`${scritUrl}stock-heatmap.js`}
+                        scriptUrl={`${scriptUrl}stock-heatmap.js`}
                         config={HEATMAP_WIDGET_CONFIG}
                         className="custom-chart"
                         height={600}
@@ -35,14 +35,14 @@ const Page = () => {
             <section className= "grid w-full gap-8 home-section">
                 <div className="h-full md:col-span-1 xl:col-span-1">
                     <TradingViewWidget
-                        scriptUrl={`${scritUrl}timeline.js`}
+                        scriptUrl={`${scriptUrl}timeline.js`}
                         config={TOP_STORIES_WIDGET_CONFIG}
                         height={600}
                     />
                 </div>
                 <div className="h-full md:col-span-1 xl:col-span-2">
                     <TradingViewWidget
-                        scriptUrl={`${scritUrl}market-quotes.js`}
+                        scriptUrl={`${scriptUrl}market-quotes.js`}
                         config={MARKET_DATA_WIDGET_CONFIG}
                         height={600}
                     />
